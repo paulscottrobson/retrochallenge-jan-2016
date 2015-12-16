20d325bb779697b08d2db3fad8c2d08b7d9dc60a
Paul Robson <paulscottrobson@gmail.com>
2015-12-16 16:35:32 +0000

    Minol Backtrack

--- software/expression_test/go.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-python generate2.py >tests.inc
-\mingw\bin\asw -L expression_test.asm 
-if errorlevel 1 goto exit
-\mingw\bin\p2bin -r 36864-65535 expression_test.p
-del expression_test.p
-copy /Y expression_test.bin ..\..\emulator\rom9000.bin
-..\..\emulator\wp1 $expression_test.bin
-:exit
--- software/miscellany/minol_expression/go.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-python generate.py >tests.inc
-\mingw\bin\asw -L exprtest.asm 
-if errorlevel 1 goto exit
-\mingw\bin\p2bin -r 36864-65535 exprtest.p
-del exprtest.p
-copy /Y exprtest.bin ..\..\emulator\rom9000.bin
-..\..\emulator\wp1 $exprtest.bin
-:exit
