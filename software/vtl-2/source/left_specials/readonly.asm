c832c37b0f5a846bb1a72fbbbf80429cb606304e
Paul Robson <paulscottrobson@gmail.com>
2015-12-28 17:46:14 +0000

    Changed & so & = 0 exits

--- software/vtl-2/source/left_specials/readonly.asm
+++ software/vtl-2/source/left_specials/readonly.asm
@@ -9,6 +9,20 @@
 	jmp 	__STROV_End
 
 __ST_ReadOnlyVariable:
+	xppc 	p3 													; check for '='
+	jp 		__STROV_End 										; if error, end.
+	xppc 	p3 													; evaluate rhs
+	ld 		@2(p2) 												; unstack the result.
+	csa 														; end if there was an error.
+	jp 		__STROV_End
+	ld 		-1(p2) 												; if the returned result was non zero
+	or 		-2(p2)
+	jnz 	__STROV_Error 										; report as error
+
+	lpi 	p3,BootMonitor-1 									; else crash back to monitor
+	xppc 	p3
+
+__STROV_Error:
 	ldi 	ERROR_ReadOnly										; set error to E
 	xae 
 	ccl 														; return with carry clear indicating error
