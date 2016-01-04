1d4d549b65229567e52341b7dab9227455a5d708
Paul Robson <paulscottrobson@gmail.com>
2016-01-04 18:48:31 +0000

    Added code to delete a given line

--- software/vtl-2/source/manager.asm
+++ software/vtl-2/source/manager.asm
@@ -6,6 +6,48 @@
 ; ****************************************************************************************************************
 ; ****************************************************************************************************************
 
+; ****************************************************************************************************************
+; ****************************************************************************************************************
+;
+;										Delete the line at TOS. Does not change TOS.
+;
+; ****************************************************************************************************************
+; ****************************************************************************************************************
+
+DeleteCodeLine:
+	pushp 	p3 													; save P3 on stack.
+
+	lpi 	p3,Variables 										; point P3 to the variables.
+	ld 		ProgramBase-Variables(p3) 							; load P1 with program base
+	xpal 	p1
+	ld 		ProgramBase-Variables+1(p3)
+	xpah 	p1
+__DCL_Search:
+	ld 		(p1) 												; read offset to next
+	jz 		__DCL_Exit 											; if zero, line not found, drop through
+	xae 														; save offset in E
+	ld 		1(p1) 												; check if line found (LSB first)
+	xor 	2(p2)
+	jnz 	__DCL_Next
+	ld 		2(p1) 												; then MSB
+	xor 	3(p2)
+	jz 		__DCL_Copy 											; if found, then start deletion process
+__DCL_Next:
+	ld 		@-0x80(p1) 											; add E to P1
+	jmp 	__DCL_Search 										; try next.
+;
+;	Found the line to delete, address in P1, offset to next in E
+;
+__DCL_Copy:
+	ld 		-0x80(p1) 											; get E up
+	st 		0x00(p1) 											; save in current slot
+	ld 		@1(p1) 												; bump P1
+	ld   	-1(p1) 												; keep going till copied a pair of zeros.
+	or 		-2(p1)												; which marks the end of the program.
+	jnz 	__DCL_Copy
+
+__DCL_Exit:														
+	jmp 	__UCT_NoPush 										; avoids PULLP P3/PUSHP P3
 
 ; ****************************************************************************************************************
 ; ****************************************************************************************************************
@@ -17,6 +59,7 @@
 
 UpdateCodeTop:
 	pushp 	p3 													; save return address.
+__UCT_NoPush:
 	lpi 	p3,Variables 										; point P3 to the variables.
 	ld 		ProgramBase-Variables(p3) 							; load P1 with program base
 	xpal 	p1
@@ -39,3 +82,4 @@ __UCT_End:
 	xpah 	p1
 	pullp 	p3 													; restore return
 	xppc 	p3 
+	
--- software/vtl-2/vtl-2.asm
+++ software/vtl-2/vtl-2.asm
@@ -30,13 +30,32 @@
 
 	ldi 	0 													; set direct mode
 	st 		IsRunning-Variables(p3)	
-	ldi		test/256											; set program base address.
+	ldi		0x10												; set program base address.
 	st 		ProgramBase-Variables+1(p3)
-	ldi		test&255
+	xpah 	p1
+	ldi		0x00
 	st 		ProgramBase-Variables(p3)
+	xpal 	p1 													; P1 = target address
+	lpi 	p3,test												; P3 = test code
+	xpal 	p1
+__Copy:															; copy sample code to RAM.
+	xpal 	p1
+	ld 		@1(p3) 												; copy byte
+	st 		@1(p1)
+	xpal 	p1 													; copy one whole page.
+	jnz 	__Copy 	
 
 	lpi 	p3,UpdateCodeTop-1 									; update & with correct value
 	xppc 	p3
+
+	ldi 	300/256												; push 300 on the stack.
+	st 		@-1(p2)
+	ldi 	300&255
+	st 		@-1(p2)
+
+	lpi 	p3,DeleteCodeLine-1 								; delete line at TOS.
+	xppc 	p3
+	ld 		@2(p2) 												; throw line.
 
 	lpi 	p3,ExecuteCodeLine-1								; direct execute # = 1
 	lpi 	p1,start
@@ -47,17 +66,15 @@
 wait0:jmp 	wait0
 
 test:
-	code 	10,"?=*-&"
-	code 	15,"?=\"\""
-	code 	20,"K = 32"
-	code 	30,"K = K + 1"
-	code 	40,"$=K"
-	code 	50,"# = (K < 44) * 30"
-	code 	60,"?=\"DONE\""
+	code 	100,"?=\"1\""
+	code 	200,"?=\"2\""
+	code 	300,"?=\"3\""
+	code 	400,"?=\"4\""
+	code 	500,"?=\"DONE\""
 	db 		0
 
 start:
-	db 		"# = 1",0
+	db 		"# = 111",0
 
 ; ****************************************************************************************************************
 ;													Source Files
